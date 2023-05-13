0fb860246387ff12ac5d9b5d4e25fdd48e9d6cec
Taniya <tanya.kalra200@gmail.com>
2023-05-13 20:08:36 +0530

    Answer created

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+    if(student.marks>50){
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(student){
+    if(student.marks>50){
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let failedStudent = arr.filter((student)=> student.marks >= 50 )
+  arr.splice(0, arr.length, ...failedStudent);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 4, name: "tanya", age: "21", marks: 85 },
+    { id: 5, name: "ishika", age: "20", marks: 80 },
+    { id: 6, name: "rishika", age: "23", marks: 90 },
+  ];
+
+  let concatenateArr = arr.concat(newArr);
+  console.log(concatenateArr);
 }
